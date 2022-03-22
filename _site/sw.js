f74dc6c468af6e9c21036ab3816972df8a5cb25b
Hyungjin.Park <hjinhada@gmail.com>
2022-03-22 23:38:55 +0900

    update theme

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220321.2357';
+const cacheName = 'chirpy-20220322.2338';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
